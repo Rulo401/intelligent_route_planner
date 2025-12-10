aecda1dde3959648bda5e9d76ba51b0f4844b7f4
Raúl Casamayor <raul.casamayor.navas@alumnos.upm.es>
2025-12-11 00:32:00 +0100

    Implementado hechos dinamicos, celdas conectadas y suelo de celdas

--- resources/route_reasoner.pl
+++ resources/route_reasoner.pl
@@ -1,111 +1,148 @@
-% cell(X, Y, Superficie, InfoPuerta)
-% Superficie ∈ {suelo_liso, rampa, suelo_irregular, cinta_mecanica}
-% InfoPuerta ∈ {none, door(IdPuerta)}
+:- use_module(library(clpfd)).
+% cell(X, Y) available cells
+:- dynamic cell/2.
+% door(D) existing doors
+:- dynamic door/1.
+% key(K) keys for interactive elements
+:- dynamic door/1.
+% conveyor(C, length) conveyor belts
+:- dynamic conveyor/2.
 
-cell(1,1, suelo_liso,      none).
-cell(1,2, rampa,           none).
-cell(1,3, cinta_mecanica,  none).
-cell(2,1, suelo_irregular, none).
-cell(2,2, suelo_liso,      door(p1)).
-...
+% floor_type(cell, type) -> type ∈ {liso, irregular, rampa}
+:- dynamic floor_type/2.
+% located_at(element, cell) -> element ∈ {door, key, goal}
+:- dynamic located_at/2.
+% entry(conveyor, cell)
+:- dynamic entry/2.
+% exit(conveyor, cell)
+:- dynamic exit/2.
 
-% key(IdLlave, X, Y)
-key(p1, 1,3).     % la llave p1 está en la celda (1,3)
+connected(cell(X,Y1), cell(X,Y2)) :- cell(X,Y1), cell(X,Y2), Y1 #= Y2-1.
+connected(cell(X,Y1), cell(X,Y2)) :- cell(X,Y1), cell(X,Y2), Y1 #= Y2+1.
+connected(cell(X1,Y), cell(X2,Y)) :- cell(X1,Y), cell(X2,Y), X1 #= X2+1.
+connected(cell(X1,Y), cell(X2,Y)) :- cell(X1,Y), cell(X2,Y), X1 #= X2-1.
+% connected(A, B) :- conveyor(C, _), entry(C, A), exit(C, B).
 
+% compatible(LoadType, FloorType)
+%   -> LoadType ∈ {standard, fragile, biochemical, dangerous}
+%   -> FloorType ∈ {smooth, uneven, slope}
+compatible(standard, smooth).
+compatible(standard, uneven).
+compatible(standard, slope).
 
-% allowed_surface(TipoMercancia, Superficie).
+compatible(fragile, smooth).
+compatible(fragile, slope).
 
-% Por suelo liso puedes ir con todo
-allowed_surface(_, suelo_liso).
+compatible(biochemical, smooth).
+compatible(biochemical, uneven).
+compatible(biochemical, slope).
 
-% Por cinta mecánica puedes ir con todo
-allowed_surface(_, cinta_mecanica).
-
-% Por rampa solo estandar, bioquimico y fragil
-allowed_surface(estandart, rampa).
-allowed_surface(bioquimico, rampa).
-allowed_surface(fragil,     rampa).
-
-% Por suelo irregular solo estandar y bioquimico
-allowed_surface(estandart,    suelo_irregular).
-allowed_surface(bioquimico,   suelo_irregular).
-
-% door_ok(InfoPuerta, LlavesQueLlevo).
-
-door_ok(none, _).
-door_ok(door(Id), Keys) :-
-    member(Id, Keys).
-
-% Recoger llaves al entrar en una celda
-
-update_keys(X, Y, KeysIn, KeysOut) :-
-    findall(K, key(K, X, Y), NewKeys),
-    append(KeysIn, NewKeys, All),
-    sort(All, KeysOut).   % elimina duplicados y ordena
-
-% neighbor(+X, +Y, -NX, -NY) : (NX,NY) es vecina de (X,Y)
-
-neighbor(X, Y, NX, Y) :-
-    NX is X + 1,
-    cell(NX, Y, _, _).
-
-neighbor(X, Y, NX, Y) :-
-    NX is X - 1,
-    cell(NX, Y, _, _).
-
-neighbor(X, Y, X, NY) :-
-    NY is Y + 1,
-    cell(X, NY, _, _).
-
-neighbor(X, Y, X, NY) :-
-    NY is Y - 1,
-    cell(X, NY, _, _).
-
-% cost_surface(Superficie, CostePaso).
-cost_surface(suelo_liso,      1).
-cost_surface(cinta_mecanica,  0.5).  % ejemplo: más rápida
-cost_surface(rampa,           2).
-cost_surface(suelo_irregular, 3).
+compatible(dangerous, smooth).
 
 
-% step(+Mercancia, +Keys, +X, +Y, -NX, -NY, -Coste)
-
-step(Mercancia, Keys, X, Y, NX, NY, Cost) :-
-    neighbor(X, Y, NX, NY),
-    cell(NX, NY, Surface, DoorInfo),
-    allowed_surface(Mercancia, Surface),
-    door_ok(DoorInfo, Keys),
-    cost_surface(Surface, Cost).
+% passable(cell(X,Y))
+passable(cell(X,Y), LoadType) :- cell(X,Y), floor_type(cell(X,Y), FloorType), compatible(LoadType, FloorType).
 
 
-% path(+Mercancia, +X, +Y, +GX, +GY, +KeysIn, +Visitados, -Camino, -CosteTotal).
 
-% Caso base: ya estamos en el destino
-path(_, X, Y, X, Y, Keys, _, [(X,Y)], 0) :-
-    % recogemos posibles llaves del destino
-    update_keys(X, Y, Keys, _).
+% ---------------------------- BORRADORES ----------------------------------
 
-% Caso recursivo
-path(Mercancia, X, Y, GX, GY, KeysIn, Visited, [(X,Y) | RestPath], TotalCost) :-
+% path(cell(X1,Y1), cell(X2,Y2), LoadType) :- 
+% route(cell(X,Y), LoadType) :- located_at(robot, cell(RX,RY)), path(cell(RX,RY), cell(X,Y), LoadType).
 
-    % al entrar en (X,Y), recogemos llaves
-    update_keys(X, Y, KeysIn, Keys1),
+% % allowed_surface(TipoMercancia, Superficie).
 
-    % movimiento a una celda vecina
-    step(Mercancia, Keys1, X, Y, NX, NY, StepCost),
+% % Por suelo liso puedes ir con todo
+% allowed_surface(_, suelo_liso).
 
-    \+ member((NX,NY), Visited),            % evitar ciclos simples
+% % Por cinta mecánica puedes ir con todo
+% allowed_surface(_, cinta_mecanica).
 
-    path(Mercancia, NX, NY, GX, GY, Keys1,
-         [(NX,NY) | Visited], RestPath, RestCost),
+% % Por rampa solo estandar, bioquimico y fragil
+% allowed_surface(estandart, rampa).
+% allowed_surface(bioquimico, rampa).
+% allowed_surface(fragil,     rampa).
 
-    TotalCost is StepCost + RestCost.
+% % Por suelo irregular solo estandar y bioquimico
+% allowed_surface(estandart,    suelo_irregular).
+% allowed_surface(bioquimico,   suelo_irregular).
 
-% shortest_route(+Mercancia, +SX, +SY, +GX, +GY, -Camino, -Coste)
+% % door_ok(InfoPuerta, LlavesQueLlevo).
 
-shortest_route(Mercancia, SX, SY, GX, GY, BestPath, BestCost) :-
-    setof(Cost-Path,
-          path(Mercancia, SX, SY, GX, GY,
-               [],         % sin llaves al inicio
-               [(SX,SY)],  % visitados
-               Path, Cost),[BestCost-BestPath | _]).
+% door_ok(none, _).
+% door_ok(door(Id), Keys) :-
+%     member(Id, Keys).
+
+% % Recoger llaves al entrar en una celda
+
+% update_keys(X, Y, KeysIn, KeysOut) :-
+%     findall(K, key(K, X, Y), NewKeys),
+%     append(KeysIn, NewKeys, All),
+%     sort(All, KeysOut).   % elimina duplicados y ordena
+
+% % neighbor(+X, +Y, -NX, -NY) : (NX,NY) es vecina de (X,Y)
+
+% neighbor(X, Y, NX, Y) :-
+%     NX is X + 1,
+%     cell(NX, Y, _, _).
+
+% neighbor(X, Y, NX, Y) :-
+%     NX is X - 1,
+%     cell(NX, Y, _, _).
+
+% neighbor(X, Y, X, NY) :-
+%     NY is Y + 1,
+%     cell(X, NY, _, _).
+
+% neighbor(X, Y, X, NY) :-
+%     NY is Y - 1,
+%     cell(X, NY, _, _).
+
+% % cost_surface(Superficie, CostePaso).
+% cost_surface(suelo_liso,      1).
+% cost_surface(cinta_mecanica,  0.5).  % ejemplo: más rápida
+% cost_surface(rampa,           2).
+% cost_surface(suelo_irregular, 3).
+
+
+% % step(+Mercancia, +Keys, +X, +Y, -NX, -NY, -Coste)
+
+% step(Mercancia, Keys, X, Y, NX, NY, Cost) :-
+%     neighbor(X, Y, NX, NY),
+%     cell(NX, NY, Surface, DoorInfo),
+%     allowed_surface(Mercancia, Surface),
+%     door_ok(DoorInfo, Keys),
+%     cost_surface(Surface, Cost).
+
+
+% % path(+Mercancia, +X, +Y, +GX, +GY, +KeysIn, +Visitados, -Camino, -CosteTotal).
+
+% % Caso base: ya estamos en el destino
+% path(_, X, Y, X, Y, Keys, _, [(X,Y)], 0) :-
+%     % recogemos posibles llaves del destino
+%     update_keys(X, Y, Keys, _).
+
+% % Caso recursivo
+% path(Mercancia, X, Y, GX, GY, KeysIn, Visited, [(X,Y) | RestPath], TotalCost) :-
+
+%     % al entrar en (X,Y), recogemos llaves
+%     update_keys(X, Y, KeysIn, Keys1),
+
+%     % movimiento a una celda vecina
+%     step(Mercancia, Keys1, X, Y, NX, NY, StepCost),
+
+%     \+ member((NX,NY), Visited),            % evitar ciclos simples
+
+%     path(Mercancia, NX, NY, GX, GY, Keys1,
+%          [(NX,NY) | Visited], RestPath, RestCost),
+
+%     TotalCost is StepCost + RestCost.
+
+% % shortest_route(+Mercancia, +SX, +SY, +GX, +GY, -Camino, -Coste)
+
+% shortest_route(Mercancia, SX, SY, GX, GY, BestPath, BestCost) :-
+%     setof(Cost-Path,
+%           path(Mercancia, SX, SY, GX, GY,
+%                [],         % sin llaves al inicio
+%                [(SX,SY)],  % visitados
+%                Path, Cost),[BestCost-BestPath | _]).
